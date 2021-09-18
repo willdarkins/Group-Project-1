7ad084a1378711384a6a5900e1e34bef5d84f02c
Matteo Meza <matteomeza@gmail.com>
2021-09-17 17:08:12 -0700

    add date and vaxDateApi variables and second fetch request

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
-var userState = "California";
-var vaxCoverageApi = "https://disease.sh/v3/covid-19/vaccine/coverage/states/" + userState + "?lastdays=30&fullData=false";
+var userState = "Oregon";
+var vaxCoverageApi = "https://disease.sh/v3/covid-19/vaccine/coverage/states/" + userState + "?lastdays=30&fullData=true";
+var date = "1";
+var vaxDateApi = "https://disease.sh/v3/covid-19/vaccine/coverage/states/" + userState + "?lastdays=" + date + "&fullData=true"
  
+
+
 fetch(vaxCoverageApi).then(function (response) {
     if (response.ok) {
         response.json().then(function (data) {
+            //console.log(data.date);
+            //console.log(data.timeline.total);
+            //console.log(vaxDateApi);
+        });
+    }
+});
+
+fetch(vaxDateApi).then(function (response) {
+    if (response.ok) {
+        response.json().then(function (data) {
+            //console.log(data.date);
+            //console.log(data.timeline.total);
             console.log(data);
         });
     }
