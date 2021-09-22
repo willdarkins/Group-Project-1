46a4d03cb18a36e0d2b3458a6bc0346daa2aadb1
Matteo Meza <matteomeza@gmail.com>
2021-09-21 19:44:27 -0700

    chose correct api

--- js/script.js
+++ js/script.js
@@ -1,30 +1,20 @@
-var userState = "Oregon";
-var vaxCoverageApi = "https://disease.sh/v3/covid-19/vaccine/coverage/states/" + userState + "?lastdays=30&fullData=true";
-var date = "1";
-var vaxDateApi = "https://disease.sh/v3/covid-19/vaccine/coverage/states/" + userState + "?lastdays=" + date + "&fullData=true"
+// var userState = "Oregon";
+// var vaxCoverageApi = "https://disease.sh/v3/covid-19/vaccine/coverage/states/" + userState + "?lastdays=30&fullData=true";
+// var date = "12";
+// var vaxDateApi = "https://disease.sh/v3/covid-19/vaccine/coverage/states/" + userState + "?lastdays=" + date + "&fullData=true"
+var vaxCoverageApi ="https://disease.sh/v3/covid-19/vaccine/coverage/states?lastdays=15&fullData=true"
  
 
 
 fetch(vaxCoverageApi).then(function (response) {
     if (response.ok) {
         response.json().then(function (data) {
-            //console.log(data.date);
-            //console.log(data.timeline.total);
-            //console.log(vaxDateApi);
+            console.log(data)
         });
     }
 });
 
-fetch(vaxDateApi).then(function (response) {
-    if (response.ok) {
-        response.json().then(function (data) {
-            //console.log(data.date);
-            //console.log(data.timeline.total);
-            console.log(data);
-        });
-    }
-});
-        
+
 
     
    
